714b0f13b34438900db670e72dea8f62bfe2b7d3
test <test@test.com>
2024-01-15 18:40:09 +0900

    아이디 중복, 회원가입, application 수정

--- src/main/java/com/sns/user/UserRestController.java
+++ src/main/java/com/sns/user/UserRestController.java
@@ -3,22 +3,66 @@ package com.sns.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sns.common.EncryptUtils;
+import com.sns.user.bo.UserBO;
+import com.sns.user.entity.UserEntity;
 
 @RestController
 @RequestMapping("/user")
 public class UserRestController {
 
+	@Autowired
+	private UserBO userBO;
+	
 	@RequestMapping("/is-duplicated-id")
 	public Map<String, Object> isDuplicatedId(
 			@RequestParam("loginId") String loginId ) {
 		
 		// DB조회
+		UserEntity user = userBO.getUserEntityByLoginId(loginId);
 		Map<String, Object> result = new HashMap<>();
-		result.put("code", 200);
-		result.put("is_duplicated_id", true);
+		
+		if (user != null) { // 중복
+			result.put("code", 200);
+			result.put("is_duplicated_id", true);
+		} else { // 중복 아님
+			result.put("code", 200);
+			result.put("is_duplicated_id", false);
+		}
+		
+		return result;
+		
+	}
+	
+	@PostMapping("/sign-up")
+	public Map<String, Object> signUp(
+			@RequestParam("loginId") String loginId, 
+			@RequestParam("password") String password, 
+			@RequestParam("name") String name, 
+			@RequestParam("email") String email ) {
+		
+		// password hashing (md5)
+		// 74b8733745420d4d33f80c4663dc5e5
+		String hashedPassword = EncryptUtils.md5(password);
+		
+		// db insert
+		Integer userId = userBO.addUser(loginId, hashedPassword, name, email);
+		
+		// 응답
+		Map<String, Object> result = new HashMap<>();
+		if (userId != null) {
+			result.put("code", 200);
+			result.put("result", "성공");
+		} else {
+			result.put("code", 500);
+			result.put("error_message", "회원가입에 실패했습니다.");
+		}
 		
 		return result;
 		
---
+++ src/main/java/com/sns/user/bo/UserBO.java
@@ -0,0 +1,37 @@
+package com.sns.user.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sns.user.entity.UserEntity;
+import com.sns.user.repository.UserRepository;
+
+@Service
+public class UserBO {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	// 로그인 id 중복확인을 위한 db 확인
+	// input: loginId	output: userEntity
+	public UserEntity getUserEntityByLoginId(String loginId) {
+		return userRepository.findByLoginId(loginId);
+	}
+	
+	// 회원가입 - db insert
+	public Integer addUser(String loginId, String password, String name, String email) {
+		
+		UserEntity userEntity = userRepository.save(
+					UserEntity.builder()
+						.loginId(loginId)
+						.password(password)
+						.name(name)
+						.email(email)
+						.build()
+				);
+		
+		return userEntity == null ? null : userEntity.getId();
+		
+	}
+	
+}
